bf86fbabfde15fdcf252c836d40af8332ab962a8
Vamshi K Ponnapalli <vamshi.ponnapalli@gmail.com>
2025-10-08 04:55:38 -0400

    Use ruff to format python files

--- lua/plugins/format.lua
+++ lua/plugins/format.lua
@@ -1,27 +1,29 @@
 return {
-	"stevearc/conform.nvim",
-	keys = {
-		{
-			"<leader>bf",
-			function()
-				require("conform").format()
-			end,
-			desc = "Format buffer",
+	{
+		"stevearc/conform.nvim",
+		keys = {
+			{
+				"<leader>bf",
+				function()
+					require("conform").format()
+				end,
+				desc = "Format buffer",
+			},
 		},
-	},
-	opts = {
-		formatters_by_ft = {
-			lua = { "stylua" },
-			python = { "ruff" },
-			javascript = { "prettierd" },
-			typescript = { "prettierd" },
-			rust = { "rustfmt" },
-			html = { "prettierd" },
-			css = { "prettierd" },
-			markdown = { "prettierd" },
-			sh = { "shfmt" },
-			zsh = { "shfmt" },
-			bash = { "shfmt" },
+		opts = {
+			formatters_by_ft = {
+				lua = { "stylua" },
+				python = { "ruff_format", "ruff_organize_imports" },
+				javascript = { "prettierd" },
+				typescript = { "prettierd" },
+				rust = { "rustfmt" },
+				html = { "prettierd" },
+				css = { "prettierd" },
+				markdown = { "prettierd" },
+				sh = { "shfmt" },
+				zsh = { "shfmt" },
+				bash = { "shfmt" },
+			},
 		},
 	},
 }
