e69bb1367092c75c739352ae65a60e6d797891da
Vamshi K Ponnapalli <vamshi.ponnapalli@gmail.com>
2026-02-10 18:15:57 -0500

    Add image rendering support via image.nvim

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -62,4 +62,52 @@ return {
 			},
 		},
 	},
+	{
+		"3rd/image.nvim",
+		event = "VeryLazy",
+		keys = {
+			{
+				"<leader>ii",
+				function()
+					local image = require("image")
+					if image.is_enabled() then
+						image.disable()
+					else
+						image.enable()
+					end
+				end,
+				desc = "Toggle image rendering",
+			},
+		},
+		dependencies = {
+			{
+				"nvim-treesitter/nvim-treesitter",
+				build = ":TSUpdate",
+				config = function()
+					require("nvim-treesitter.configs").setup({
+						ensure_installed = { "markdown" },
+						highlight = { enable = true },
+					})
+				end,
+			},
+		},
+		opts = {
+			backend = "kitty",
+			integrations = {
+				markdown = {
+					enabled = true,
+					clear_in_insert_mode = false,
+					download_remote_images = true,
+					only_render_image_at_cursor = false,
+					only_render_image_at_cursor_mode = "inline",
+					filetypes = { "markdown" }, -- markdown extensions (ie. quarto) can go here
+				},
+			},
+			max_width = nil,
+			max_height = nil,
+			max_width_window_percentage = nil,
+			max_height_window_percentage = nil,
+			kitty_method = "normal",
+		},
+	},
 }
